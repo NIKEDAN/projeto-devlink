1f8fae3ef3e06662e13d26ca7b0550d247262867
NIKEDAN <lucasnnbz.hotmail@gmail.com>
2023-10-05 23:33:34 -0300

    .

--- script.js
+++ script.js
@@ -8,14 +8,14 @@ function toggleMode() {
 
     // Substituir a imagem
 
-    if (html.classList.contains('light')){
+    if (html.classList.contains("light")){
         // se tiver lightmode add img ligth
-       img.setAttribute('src', 'img/avatar-light.png')
-       img.setAttribute('alt', 'Foto Simples de Lucas Oliveira')
+       img.setAttribute("src", "img/avatar-light.png")
+       img.setAttribute("alt", "Foto Simples de Lucas Oliveira")
     } else {
         // se tiver sem lightmode manter a  img
-        img.setAttribute('src', 'img/Avatar.png')
-        img.setAttribute('alt', 'Foto de Lucas Oliveira, usando óculos e sem camisa e fundo de sítio')
+        img.setAttribute("src", "img/Avatar.png")
+        img.setAttribute("alt", "Foto de Lucas Oliveira, usando óculos'")
     }
 
 
