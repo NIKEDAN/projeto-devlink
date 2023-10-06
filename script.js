04fa6677916b6ce9f0d591724a155f3523b99293
NIKEDAN <lucasnnbz.hotmail@gmail.com>
2023-10-05 23:11:22 -0300

    Trocando Extensão da img

--- script.js
+++ script.js
@@ -10,11 +10,11 @@ function toggleMode() {
 
     if (html.classList.contains('light')){
         // se tiver lightmode add img ligth
-       img.setAttribute('src', 'img/avatar2.png')
+       img.setAttribute('src', 'img/avatar2.jpg')
        img.setAttribute('alt', 'Foto Simples de Lucas Oliveira')
     } else {
         // se tiver sem lightmode manter a  img
-        img.setAttribute('src', 'img/avatar.png')
+        img.setAttribute('src', 'img/avatar.jpg')
         img.setAttribute('alt', 'Foto de Lucas Oliveira, usando óculos e sem camisa e fundo de sítio')
     }
 
